19f3c2e7e0adf05145f2d18504f1a7c6d4eb0e30
Alex Ford <alexford@frontside.io>
2016-08-12 13:23:30 -0500

    Migrate empty test cases over

---
+++ tests/integration/components/x-field-test.js
@@ -0,0 +1,27 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describeComponent, it } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describeComponent(
+  'x-field',
+  'Integration: XFieldComponent',
+  {
+    integration: true
+  },
+  function() {
+    it('renders', function() {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.on('myAction', function(val) { ... });
+      // Template block usage:
+      // this.render(hbs`
+      //   {{#x-field-test}}
+      //     template content
+      //   {{/x-field-test}}
+      // `);
+
+      this.render(hbs`{{x-field}}`);
+      expect(this.$()).to.have.length(1);
+    });
+  }
+);
---
+++ tests/unit/utils/apply-changeset-test.js
@@ -0,0 +1,16 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describe, it, beforeEach } from 'mocha';
+import applyChangeset from 'emberx-form/utils/apply-changeset';
+
+describe('applyChangeset', function() {
+  // Replace this with your real tests.
+  beforeEach(function() {
+    let changeset = {};
+    this.result = applyChangeset(changeset);
+  });
+
+  it('works', function() {
+    expect(this.result).to.be.ok;
+  });
+});
