8c1eae154f8b23586cca95ffc338fe0697d91333
Alex Ford <alexford@frontside.io>
2016-08-12 11:24:07 -0500

    Testing Submissions and Errors with Promises

--- tests/integration/components/x-form-test.js
+++ tests/integration/components/x-form-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -14,11 +15,12 @@ test('it cannot be instantiated without data', function(assert) {
   assert.ok(err, 'x-form cannot render without data attribute');
 });
 
-test('it renders with data', function(assert) {
-  assert.expect(1);
+test('rendering the blockless form with data', function(assert) {
+  assert.expect(2);
   let err = null;
   try { this.render(hbs`{{x-form data=(hash)}}`); } catch(e) { err = e; }
-  assert.notOk(err, 'x-form renders with a data attribute');
+  assert.equal(this.$('form').length, 1, 'it renders the form');
+  assert.notOk(err, 'it renders x-form without errors');
 });
 
 test('observing form actions', function(assert) {
@@ -43,7 +45,82 @@ test('observing form actions', function(assert) {
 
   this.$('.submit').click();
   this.$('.cancel').click();
+});
 
+test('submission is in flight', function(assert) {
+  assert.expect(2);
+  this.set('data', SimpleData);
+
+  this.set('submit', () => new Ember.RSVP.Promise(() => {}) );
+
+  // Template block usage:
+  this.render(hbs`
+    {{#x-form
+       data=data
+       onSubmit=submit
+       onSuccess=success
+       as |form|
+    }}
+      <button class='submit' {{action form.actions.onSubmit}}>Submit</button>
+      <div class={{if form.isSubmitting "in-flight"}}></div>
+    {{/x-form}}
+  `);
+
+  assert.notOk(this.$('.in-flight').length);
+
+  this.$('.submit').click(); // Triggers an unresolved promise
+
+  assert.ok(this.$('.in-flight').length);
+});
+
+test('success on submit', function(assert) {
+  assert.expect(1);
+  this.set('data', SimpleData);
+
+  this.set('submit', () => Ember.RSVP.Promise.resolve("Success") );
+  this.set('success', () => {
+    assert.notOk(this.$('.in-flight').length, 'the records is not in flight');
+  });
+
+  // Template block usage:
+  this.render(hbs`
+    {{#x-form
+       data=data
+       onSubmit=submit
+       onSuccess=success
+       as |form|
+    }}
+      <button class='submit' {{action form.actions.onSubmit}}>Submit</button>
+      <div class={{if form.isSubmitting "in-flight"}}></div>
+    {{/x-form}}
+  `);
+
+  this.$('.submit').click();
+});
+
+test('error on submit', function(assert) {
+  assert.expect(1);
+  this.set('data', SimpleData);
+
+  this.set('submit', () => Ember.RSVP.Promise.reject("Error") );
+  this.set('error', () => {
+    assert.notOk(this.$('.in-flight').length, 'the records is not in flight');
+  });
+
+  // Template block usage:
+  this.render(hbs`
+    {{#x-form
+       data=data
+       onSubmit=submit
+       onError=error
+       as |form|
+    }}
+      <button class='submit' {{action form.actions.onSubmit}}>Submit</button>
+      <div class={{if form.isSubmitting "in-flight"}}></div>
+    {{/x-form}}
+  `);
+
+  this.$('.submit').click();
 });
 
 // test('simple validations', function(assert) {
