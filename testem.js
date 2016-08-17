5c4802ebf33fd626c7c6f8c7e304ad82633434e4
Robdel12 <robertdeluca19@gmail.com>
2016-08-16 22:06:31 -0500

    Add brefore install commands for chrome & travis

--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 /*jshint node:true*/
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
+  "framework": "mocha",
+  "test_page": "tests/index.html",
   "disable_watching": true,
   "launch_in_ci": [
     "Chrome"
