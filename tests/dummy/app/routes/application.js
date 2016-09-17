9490143355199031ff9cc3fee1ceab893c4c9825
Chris Freeman <cfreeman.code@gmail.com>
2016-09-17 14:13:54 -0500

    Update dummy app so we can test more scenarios
    Ember Object vs POJO

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {
+    let obj = {
       firstName: '',
       lastName: '',
       birthday: new Date(1986, 8, 18),
       favoriteBand: 'Sonic Youth'
+    };
+
+    let emberObj = Ember.Object.create(obj);
+
+    return {
+      obj,
+      emberObj
     };
   }
 });
