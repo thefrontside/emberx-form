462d5ea3cb40b6f51d74dede32a4b655557c6a08
Chris Freeman <cfreeman.code@gmail.com>
2016-10-01 12:27:03 -0500

    add some comments to the page object

--- tests/pages/people.js
+++ tests/pages/people.js
@@ -10,6 +10,7 @@ import {
   text
 } from 'ember-cli-page-object';
 
+// Make a basic page object component and merge in options
 const makeComponent = (selector, options) => {
   let baseComponent = {
     scope: selector
@@ -18,6 +19,7 @@ const makeComponent = (selector, options) => {
   return Object.assign({}, baseComponent, options);
 }
 
+// Helper for creating button components that can be disabled
 const makeButton = (selector) => {
   let options = {
     isDisabled: is(':disabled')
@@ -26,6 +28,11 @@ const makeButton = (selector) => {
   return makeComponent(selector, options);
 }
 
+// Helper for making form-field components.
+// fieldSelector: the entire form-group
+// inputSelector: the actual form input element (could be input, select box, etc.)
+// action: a tuple containing the action name and the page object helper you'd like to call, e.g.
+//   `fillable`, `selectable`, etc.
 const makeField = (fieldSelector, inputSelector, [name, action]) => {
   let options = {
     hasErrors: hasClass('has-error'),
@@ -33,6 +40,9 @@ const makeField = (fieldSelector, inputSelector, [name, action]) => {
     [name]: action(inputSelector),
     blur: triggerable('blur', inputSelector),
 
+    // This create a composite action that will call the action defined above, and then trigger
+    // the blur event
+    // In your test, use this as (for example) `pageObject.field.fillInAndBlur`
     [`${name}AndBlur`](value) {
       this[name](value);
       this.blur();
